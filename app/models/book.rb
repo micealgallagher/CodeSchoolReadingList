176064c1c7b6e9f05c2dd11df5c4e35586457cfd
micealgallagher <me@mehaul.me>
2016-04-02 12:54:06 -0400

    Add Genre and create junction table to like genres to books. Completed video to 1:25:50

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,10 +1,11 @@
 class Book < ActiveRecord::Base
-	scope :finished, -> { where.not(finished_on: nil) }
-	# scope :recent, 	 -> { where('finished_on > ?', 2.days.ago) }
 
-	def self.recent
-		where('finished_on > ?', 2.days.ago)
-	end
+	has_many :book_genres
+	has_many :genres, through: :book_genres
+
+	scope :finished, -> { where.not(finished_on: nil) }
+	scope :recent,   -> { where('finished_on > ?', 2.days.ago) }
+	scope :search,   -> (keyword) { where('keywords LIKE ?', "%#{keyword.downcase}%") if keyword.present }
 
 	def finished?
 		finished_on.present?
